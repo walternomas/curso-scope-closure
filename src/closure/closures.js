6928a4fb24bdcaf2377bab37d0acc111e031119f
walternomas <w.barriosvazquez@gmail.com>
2022-08-09 19:19:46 -0300

    closure & lexic environment

--- src/closure/closures.js
+++
@@ -1,6 +0,0 @@
-/**
- * CLOSURES
- * Tienes una closure cuando una función cualquiera accede
- * a una variable fuera de su contexto.
- */
-
---
+++ src/closure/mymoneybox.js
@@ -0,0 +1,28 @@
+// function moneyBox(coins) {
+//   let saveCoins = 0;
+//   saveCoins += coins;
+//   console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// moneyBox(5);
+// moneyBox(5);
+// moneyBox(5);
+
+function moneyBox() {
+  let saveCoins = 0;
+  function countCoins(coins) {
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+  }
+  return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(5);
+
+const moneyBoxJaz = moneyBox();
+moneyBoxJaz(10);
+moneyBoxJaz(20);
+moneyBoxJaz(5);
